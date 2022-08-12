6d395a5294cd7e7df743dc80a20657ea13579a8a
KoHsuanNa <97872578+KoHsuanNa@users.noreply.github.com>
2022-08-12 17:11:30 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=content))
     if re.match('使用說明',message):
         line_bot_api.reply_message(event.reply_token,TextSendMessage('基本上以圖文選單操作  \n*輸入：一分鐘-->1分鐘後回傳：一分鐘到了！  \n*輸入：5分鐘後提醒我-->5分鐘後回傳：5分鐘到了！  \n*輸入：設定到站提醒-->出現2分鐘選項-->按下會自動發送2分鐘後提醒我-->2分鐘後回傳：2分鐘到了！  \n*輸入：使用者 -->回傳：userid'))
-        '''
+        
     if re.match('紀錄',message):
         try:
             record_list = prepare_record(message)
@@ -116,7 +116,7 @@ def handle_message(event):
          line_bot_api.reply_message(event.reply_token,TextSendMessage(text=result))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage('呵呵'))
-'''
+
 
 #主程式
 import os
