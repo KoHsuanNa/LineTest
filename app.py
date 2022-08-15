7470a9b05410d71ba9c7199082885bce86c59636
KoHsuanNa <97872578+KoHsuanNa@users.noreply.github.com>
2022-08-15 17:04:56 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ a = 2
 dt1 = datetime.utcnow().replace(tzinfo=timezone.utc)
 dt2 = dt1.astimezone(timezone(timedelta(hours=8)))
 timenow = dt2.strftime("%Y-%m-%d %H:%M:%S")
-time_2 = '2022-08-15 17:20:00'                                      #設定預計抵達時間
+time_2 = '2022-08-15 17:10:00'                                      #設定預計抵達時間
 time_1_struct = datetime.strptime(timenow, "%Y-%m-%d %H:%M:%S")     #現在時間
 time_2_struct = datetime.strptime(time_2, "%Y-%m-%d %H:%M:%S")      #預計抵達時間
 seconds = (time_2_struct - time_1_struct).seconds                   #相差的秒數
@@ -91,8 +91,8 @@ def handle_message(event):
                          ))]))
          line_bot_api.reply_message(event.reply_token, image_carousel_template_message)
     if re.match('欸欸提醒我',message):
-        #time.sleep(seconds-120)
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('不要'))
+        time.sleep(seconds-120)
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('準備下車'))
     
     if re.match('使用者',message):
         user_id = event.source.user_id
